4eb180b73a140e1ac6b01ec2730cb2275789f4cd
masayuki.inoue <macha-sport0412810@docomo.ne.jp>
2021-11-02 22:01:31 +0900

    destroyServlet 修正

--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -41,7 +41,7 @@ public class DestroyServlet extends HttpServlet {
                 em.getTransaction().commit();
                 em.close();
 
-                request.getSession().removeAttribute("message_id");
+                request.getSession().removeAttribute("task_id");
 
                 response.sendRedirect(request.getContextPath() + "/index");
             }
